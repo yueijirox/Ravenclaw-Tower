5d8de6483bb0d4558b336a549fa224b8e407e12f
yueijirox <branislavchrysanthos@gmail.com>
2023-01-24 14:41:12 +0700

    ADDED TO FILE

---
+++ w2/packA/Accountant.java
@@ -0,0 +1,40 @@
+package packA;
+public class Accountant extends Programmer{
+    private static String companyName="berk barn jamkad";
+    private int experience;
+    private String specialty;
+
+
+
+    public Accountant(String name,int superExp,int experience,int sal,String talent){
+        super(name,superExp,sal);
+        this.experience=experience;
+        specialty=talent;
+    }
+
+    public void setSpecialty(String newSpecialty){
+        specialty=newSpecialty;
+    }
+
+    public int getAccountExperience(){
+        return experience;
+    }
+
+    public String tellProfit(){
+        return companyName+"'s profit is "+(int) (Math.random()*1000) +" My salary is "+getSalary(); 
+    }
+
+    public String toString() {
+        return "Accountant[experience=" + experience + ", specialty=" + specialty
+                + "]";
+    }
+    
+    public void sayHi(){
+        System.out.println(getName()+" say hello ");
+    }
+
+    public static String tellMyRole(){
+        return "I am an accountant at " + companyName;
+    } 
+    
+}
